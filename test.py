7325ad18809120ca18b511ac3a62e836332c6908
Moritz Schott <moritz.schott@uni-heidelberg.de>
2022-07-18 11:46:21 +0200

    explicitly recurse down

---
+++ test.py
@@ -0,0 +1,3 @@
+import osm_to_sandbox.osm_to_sandbox
+
+osm_to_sandbox.osm_to_sandbox.main([8.6682651734,49.4176972798,8.669646511,49.4182364458],osm_to_sandbox.osm_to_sandbox.read_auth(),filter_str="building=university")
